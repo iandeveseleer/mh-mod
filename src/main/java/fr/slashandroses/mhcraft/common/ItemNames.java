007612ffa8dd4b287a83cb7ab02166fc6370699c
Ian DEVESELEER <contact@iandeveseleer.fr>
2021-01-17 13:12:34 +0100

    1.2.0 : Add _EnAttendantAllotropique discs

--- src/main/java/fr/slashandroses/mhcraft/common/ItemNames.java
+++ src/main/java/fr/slashandroses/mhcraft/common/ItemNames.java
@@ -6,4 +6,8 @@ public class ItemNames {
     public static final String GRAY_BRICK = "gray_brick";
     public static final String MARIJUANA = "marijuana";
     public static final String MARIJUANA_SEEDS = "marijuana_seeds";
+    public static final String EN_BERLINE_DISC = "en_berline";
+    public static final String NAZARETH_DISC = "nazareth";
+    public static final String BLEUCADAVRE_DISC = "bleucadavre";
+    public static final String ADEBISI_DISC = "adebisi";
 }
--- src/main/java/fr/slashandroses/mhcraft/registry/SoundRegistry.java
+++ src/main/java/fr/slashandroses/mhcraft/registry/SoundRegistry.java
@@ -8,9 +8,22 @@ import static fr.slashandroses.mhcraft.MHCraft.registerSound;
 
 public class SoundRegistry {
     public static final Identifier GLASSCUTTING_SOUND = new Identifier(MHConstants.NAMESPACE, "glasscutting");
+    public static final Identifier EN_BERLINE_MUSIC = new Identifier(MHConstants.NAMESPACE, "en_berline");
+    public static final Identifier NAZARETH_MUSIC = new Identifier(MHConstants.NAMESPACE, "nazareth");
+    public static final Identifier BLEUCADAVRE_MUSIC = new Identifier(MHConstants.NAMESPACE, "bleucadavre");
+    public static final Identifier ADEBISI_MUSIC = new Identifier(MHConstants.NAMESPACE, "adebisi");
     public static SoundEvent GLASSCUTTING_SOUND_EVENT = new SoundEvent(GLASSCUTTING_SOUND);
+    public static SoundEvent EN_BERLINE_SOUND_EVENT = new SoundEvent(EN_BERLINE_MUSIC);
+    public static SoundEvent NAZARETH_SOUND_EVENT = new SoundEvent(NAZARETH_MUSIC);
+    public static SoundEvent BLEUCADAVRE_SOUND_EVENT = new SoundEvent(BLEUCADAVRE_MUSIC);
+    public static SoundEvent ADEBISI_SOUND_EVENT = new SoundEvent(ADEBISI_MUSIC);
+
 
     public static void init() {
         registerSound(GLASSCUTTING_SOUND, GLASSCUTTING_SOUND_EVENT);
+        registerSound(EN_BERLINE_MUSIC, EN_BERLINE_SOUND_EVENT);
+        registerSound(NAZARETH_MUSIC, NAZARETH_SOUND_EVENT);
+        registerSound(BLEUCADAVRE_MUSIC, BLEUCADAVRE_SOUND_EVENT);
+        registerSound(ADEBISI_MUSIC, ADEBISI_SOUND_EVENT);
     }
 }
